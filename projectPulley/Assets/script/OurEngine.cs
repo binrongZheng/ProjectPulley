d553c045ba97a53c8b940633b4616429260940f4
Pau Blanes <paublanespardo@enti.cat>
2017-11-29 18:02:06 +0100

    Classe Vector3

---
+++ projectPulley/Assets/script/OurEngine.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using UnityEngine;
+
+namespace ourEngine {
+
+	public class ourVector3 {
+		public float x,y,z;
+
+		public ourVector3 () {
+			x = y = z = 0;
+		}
+		public ourVector3(float _x, float _y, float _z){
+			x = _x;
+			y = _y;
+			z = _z;
+		}
+
+		public float GetMagnitude () {
+			return UnityEngine.Mathf.Sqrt (UnityEngine.Mathf.Pow (x, 2) + UnityEngine.Mathf.Pow (y, 2) + UnityEngine.Mathf.Pow (z, 2));
+		}
+
+		public static ourVector3 operator + (ourVector3 v1, ourVector3 v2){
+			ourVector3 res = new ourVector3();
+			res.x = v1.x + v2.x;
+			res.y = v1.y + v2.y;
+			res.z = v1.z + v2.z;
+			return res;
+		}
+		public static ourVector3 operator - (ourVector3 v1, ourVector3 v2){
+			ourVector3 res = new ourVector3();
+			res.x = v1.x - v2.x;
+			res.y = v1.y - v2.y;
+			res.z = v1.z - v2.z;
+			return res;
+		}
+
+		public static float Dot (ourVector3 v1, ourVector3 v2){
+			float res = v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+			return res;
+		}
+		public static ourVector3 Cross (ourVector3 v1, ourVector3 v2){
+			ourVector3 res = new ourVector3();
+			res.x = v1.y * v2.z - v1.z * v2.y;
+			res.y = v1.z * v2.x - v1.x * v2.z;
+			res.z = v1.x * v2.y - v1.y * v2.x;
+			return res;
+		}
+		public static float Distance (ourVector3 v1, ourVector3 v2){
+			ourVector3 res = v1 - v2;
+			return res.GetMagnitude();
+		}
+
+		public ourVector3 Normalize () {
+			float magnitude = GetMagnitude ();
+			ourVector3 res = new ourVector3 (x / magnitude, y / magnitude, z / magnitude);
+			return res;
+		} 
+
+	}
+}
---
+++ projectPulley/Assets/script/prova.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using UnityEngine;
+using ourEngine;
+
+public class prova : UnityEngine.MonoBehaviour {
+
+	ourVector3 provant = new ourVector3(1,0,0);
+	ourVector3 provant2 = new ourVector3(5,0,0);
+
+	// Use this for initialization
+	void Start () {
+		ourVector3 res = ourVector3.Cross (provant, provant2);
+
+		print (provant.Normalize().y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
