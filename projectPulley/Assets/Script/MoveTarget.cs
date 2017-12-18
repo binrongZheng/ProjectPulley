6e7b75710a7c269ea085a423eabe8f4f6473462d
Pau Blanes <paublanes.enti@gmail.com>
2017-12-18 21:23:59 +0100

    Moviment target i colcada camera

---
+++ projectPulley/Assets/Script/MoveTarget.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTarget : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.W) && transform.position.y < 3){
+			transform.position += new Vector3(0,2 * Time.deltaTime,0);
+		}
+		if (Input.GetKey(KeyCode.S) && transform.position.y > 0){
+			transform.position -= new Vector3(0,2 * Time.deltaTime,0);
+		}
+	}
+}
