192b9ca07f5c226dc9d03347b6ce4296afcc2c93
Pau Blanes <paublanes.enti@gmail.com>
2018-01-26 13:19:41 +0100

    Textos escena fixed pulley

---
+++ projectPulley/Assets/Script/FollowObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ourEngine;
+
+public class FollowObject : MonoBehaviour {
+
+	private Vector3 offSet;
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		offSet = (target.position - transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = target.position - offSet;
+	}
+	void LateUpdate () {
+		
+	}
+}
